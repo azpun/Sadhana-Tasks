c515b40624de5ecf8a6ff7e4a8e12f9681e3efeb
azpun <azpunari@gmail.com>
2026-04-10 09:07:28 +0700

    feat: add new hero assets and UI icons, and update landing page layout

--- src/components/layouts/LandingLayout/LandingLayout.tsx
+++ src/components/layouts/LandingLayout/LandingLayout.tsx
@@ -3,12 +3,22 @@ import Navbar from "../../common/Navbar";
 
 export const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="h-full gap-4 bg-slate-300 dark:bg-black">
       <Navbar />
       <MobileMenu />
-      <main className="h-screen gap-4 px-4 bg-slate-300 dark:bg-black">
+      <main className="h-full gap-4 px-4 bg-slate-300 dark:bg-black">
         {children}
       </main>
+      <footer className="bg-white dark:bg-gray-900 mt-10 p-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div>
+            <p>© 2026 Task Tracker. All rights reserved.</p>
+          </div>
+          <div>
+            <p>Made by Punz</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,131 @@
+import { Link } from "react-router-dom";
 import LandingLayout from "../../components/layouts/LandingLayout";
 
 export const LandingPage = () => {
+  const features = [
+    {
+      name: "Have a look at your dashboard",
+      description:
+        "See all your tasks in one place, organized by status and priority.",
+      icon: "/ui/dash.svg",
+    },
+    {
+      name: "Set up your tasks",
+      description:
+        "Set up your tasks in one place, organized by categories and priority.",
+      icon: "/ui/task.svg",
+    },
+    {
+      name: "Track your progress",
+      description:
+        "Track your progress and stay motivated with our intuitive progress tracking system.",
+      icon: "/ui/progress-bar.svg",
+    },
+    {
+      name: "Get reminders",
+      description:
+        "Get reminders for your tasks to stay on track and never miss a deadline.",
+      icon: "/ui/bell.svg",
+    },
+  ];
   return (
     <>
       <LandingLayout>
-        <div
-          className="flex flex-col items-center justify-center bg-white dark:bg-slate-900 border 
-        border-slate-300 dark:border-slate-700 rounded-2xl shadow-md h-full "
-        >
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">Landing Page</h1>
-            <p className="text-gray-600 dark:text-gray-400">
-              Track your tasks and improve your productivity
+        <div className="bg-white dark:bg-gray-900 rounded-2xl">
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+              <div className="text-center">
+                <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-white sm:text-7xl">
+                  Make your life easier with Task Tracker
+                </h1>
+                <p className="mt-8 text-lg font-medium text-pretty text-gray-600 dark:text-gray-400 sm:text-xl/8">
+                  Task Tracker is a simple and intuitive task management tool
+                  that helps you stay organized and productive.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <Link
+                    to="/login"
+                    className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-400 
+                    focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+                  >
+                    Get started
+                  </Link>
+                  <Link
+                    to="/features"
+                    className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col items-center justify-center">
+            <div className="mx-auto max-w-2xl lg:mx-0 text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                Features
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-700 dark:text-gray-400">
+                Task Tracker is a simple and intuitive task management tool that
+                helps you stay organized and productive.
+              </p>
+            </div>
+          </div>
+          <div className="mx-auto mt-12 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-16">
+                  <dt className="text-base/7 font-semibold text-black dark:text-white">
+                    <div className="absolute top-0 left-3 flex size-10 items-center justify-center rounded-lg bg-blue-500">
+                      <img src={feature.icon} alt="icon" className="size-6" />
+                    </div>
+                    {feature.name}
+                  </dt>
+                  <dd className="mt-2 text-base/7 text-gray-700 dark:text-gray-400">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+        <div className="py-12 sm:py-16 h-fit bg-white dark:bg-gray-900 rounded-2xl my-10 md:flex md:justify-between ">
+          <div className="mx-auto max-w-md text-center lg:mx-15 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="text-3xl font-semibold tracking-tight text-balance text-black dark:text-white sm:text-4xl">
+              Boost your productivity. Start using our app today.
+            </h2>
+            <p className="mt-6 text-lg/8 text-pretty text-gray-700 dark:text-gray-400">
+              Task Tracker is a simple and intuitive task management tool that
+              helps you stay organized and productive.
             </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="#"
+                className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white 
+                inset-ring inset-ring-white/5 hover:bg-blue-400 focus-visible:outline-2 
+                focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+              >
+                Get started
+              </a>
+              <a
+                href="#"
+                className="text-sm/6 font-semibold text-black dark:text-white hover:text-gray-100"
+              >
+                Learn more
+                <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+          <div className="relative h-80">
+            <img
+              alt="App screenshot"
+              src="/assets/hero1.svg"
+              width={1824}
+              height={1080}
+              className=" w-228 max-w-none rounded-md "
+            />
           </div>
         </div>
       </LandingLayout>
